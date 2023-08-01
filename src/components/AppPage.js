a12cc196f7bb852e0ed16d692713391d63b17a67
Royston Yeo <royywc91@gmail.com>
2023-08-01 22:10:52 +0800

    tmr after see doc continue

--- src/components/AppPage.js
+++ src/components/AppPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useContext, useState } from "react";
-import DispatchContext from "../DispatchContext";
-import axios from "axios";
-import EditAppModal from "./EditAppModal";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useEffect, useContext, useState } from 'react';
+import DispatchContext from '../DispatchContext';
+import axios from 'axios';
+import EditAppModal from './EditAppModal';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function AppPage() {
   const appDispatch = useContext(DispatchContext);
@@ -25,23 +25,23 @@ export default function AppPage() {
     const ourRequest = axios.CancelToken.source();
     const fetchProfile = async () => {
       try {
-        const response = await axios.get("/user/profile");
+        const response = await axios.get('/user/profile');
         if (response.data.data[0]) {
-          appDispatch({ type: "isAuth", data: response.data.data[0] });
+          appDispatch({ type: 'isAuth', data: response.data.data[0] });
         }
       } catch (error) {
         if (error.response.data) {
           appDispatch({
-            type: "flashMessageErr",
+            type: 'flashMessageErr',
             value: error.response.data.errMessage,
           });
-          navigate("/");
+          navigate('/');
         }
       }
     };
     const fetchApp = async () => {
       try {
-        const response = await axios.get("/apps");
+        const response = await axios.get('/apps');
         if (response.data.data) {
           setApps(response.data.data);
         }
@@ -54,69 +54,63 @@ export default function AppPage() {
     return () => ourRequest.cancel();
   }, []);
   return (
-    <div className="dashboard">
-      <div className="ml-5 mt-3" onClick={() => navigate(-1)}>
+    <div className='dashboard'>
+      <div className='ml-5 mt-3' onClick={() => navigate(-1)}>
         <i
-          class="fa fa-arrow-left fa-2x align-self-center"
-          aria-hidden="true"
-          style={{ cursor: "pointer" }}
+          class='fa fa-arrow-left fa-2x align-self-center'
+          aria-hidden='true'
+          style={{ cursor: 'pointer' }}
         ></i>
       </div>
-      <div className="container-fluid text-center">
-        <p className="dashboard__description text-center">All Available Apps</p>
-        <div className="d-flex justify-content-center mb-2">
+      <div className='container-fluid text-center'>
+        <p className='dashboard__description text-center'>All Available Apps</p>
+        <div className='d-flex justify-content-center mb-2'>
           <button
-            className="btn btn-outline-dark mt-2 mr-2"
+            className='btn btn-outline-dark mt-2 mr-2'
             onClick={() => {
-              navigate("/apps/create");
+              navigate('/apps/create');
             }}
           >
-            <i class="fas fa-plus"></i> Create App
+            <i class='fas fa-plus'></i> Create App
           </button>
         </div>
 
-        <div className="container-fluid row justify-content-center d-inline-flex mt-5 mx-auto">
-          <div className="table-responsive mt-2 mb-5">
-            <table className="table table-bordered table-shadow mb-0">
+        <div className='container-fluid row justify-content-center d-inline-flex mt-5 mx-auto'>
+          <div className='table-responsive mt-2 mb-5'>
+            <table className='table table-bordered table-shadow mb-0'>
               <thead>
                 <tr>
-                  <th className="bg-dark text-white">App Name</th>
-                  <th className="bg-dark text-white">App Start Date</th>
-                  <th className="bg-dark text-white">App End Date</th>
-                  <th className="bg-dark text-white">App Description</th>
-                  <th className="bg-dark text-white">Actions</th>
+                  <th className='bg-dark text-white'>App Name</th>
+                  <th className='bg-dark text-white'>App Start Date</th>
+                  <th className='bg-dark text-white'>App End Date</th>
+                  <th className='bg-dark text-white'>App Description</th>
+                  <th className='bg-dark text-white'>Actions</th>
                 </tr>
               </thead>
               <tbody>
                 {apps.map((app, idx) => (
                   <tr
                     key={idx}
-                    className={idx % 2 === 0 ? "even-row" : "odd-row"}
+                    className={idx % 2 === 0 ? 'even-row' : 'odd-row'}
                   >
-                    {/* <Link
-                      to={`/apps/${app.App_Acronym}`}
-                      style={linkStyle}
-                      key={app.id}
-                    > */}
                     <td>{app.App_Acronym}</td>
-                    {/* </Link> */}
                     <td>{app.App_startDate}</td>
                     <td>{app.App_endDate}</td>
                     <td>{app.App_Description}</td>
-                    <td className="d-flex justify-content-center">
+                    <td className='d-flex justify-content-center'>
                       <button
-                        className="btn btn-outline-dark text-center mr-2"
-                        style={{ width: "60px" }}
+                        className='btn btn-outline-dark text-center mr-2'
+                        style={{ width: '60px' }}
                         onClick={() => handleView(app.App_Acronym)}
                       >
                         View
                       </button>
                       <button
-                        className="btn btn-outline-dark text-center "
-                        style={{ width: "60px" }}
+                        className='btn btn-outline-dark text-center '
+                        style={{ width: '60px' }}
                         onClick={() => handleEdit(app.App_Acronym)}
-                        data-toggle="modal"
-                        data-target="#editAppModal"
+                        data-toggle='modal'
+                        data-target='#editAppModal'
                       >
                         Edit
                       </button>
